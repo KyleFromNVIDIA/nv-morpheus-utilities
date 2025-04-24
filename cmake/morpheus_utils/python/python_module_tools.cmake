2758c67142429571aca6fca9b64f1d7750416565
ptaylor <paul.e.taylor@me.com>
2025-04-24 12:55:47 -0700

    prepend binary dir to LD_LIBRARY_PATH so libs in the build dir can be dlopen'd by python

--- cmake/morpheus_utils/python/python_module_tools.cmake
+++ cmake/morpheus_utils/python/python_module_tools.cmake
@@ -622,7 +622,7 @@ macro(__create_python_library MODULE_NAME)
     # Before installing, create the custom command to generate the stubs
     add_custom_command(
       OUTPUT  ${module_binary_stub_file}
-      COMMAND env PYTHONPATH=${module_root_binary_dir}:$PYTHONPATH ${Python3_EXECUTABLE} -m pybind11_stubgen ${TARGET_NAME} --no-setup-py --log-level WARN -o ./ --root-module-suffix ""
+      COMMAND env LD_LIBRARY_PATH=${module_root_binary_dir}:$LD_LIBRARY_PATH PYTHONPATH=${module_root_binary_dir}:$PYTHONPATH ${Python3_EXECUTABLE} -m pybind11_stubgen ${TARGET_NAME} --no-setup-py --log-level WARN -o ./ --root-module-suffix ""
       DEPENDS ${PYTHON_ACTIVE_PACKAGE_NAME}-modules $<TARGET_OBJECTS:${TARGET_NAME}>
       COMMENT "Building stub for python module ${TARGET_NAME}..."
       WORKING_DIRECTORY ${module_root_binary_dir}
